6d1f08eae6502153bd9d43973e9e1d5dd0ad659a
AyomideKayode <ayomidekay7@gmail.com>
2024-11-30 23:52:47 +0100

    Refactor navbar functionality and styles; implement mobile navigation toggle; enhance header behavior on scroll.🚧

--- v3/public/js/script.js
+++ v3/public/js/script.js
@@ -14,4 +14,41 @@ const loadingElement = document.querySelector('[data-loading]'); // get loading 
 window.addEventListener('load', function () {
   loadingElement.classList.add('loaded'); // add loaded class to loading element
   this.document.body.classList.remove('active'); // remove active class from body
-})
+});
+
+// Mobile Nav Toggle Function
+const [navTogglers, navLinks, navbar, overlay] = [
+  document.querySelectorAll('[data-nav-toggler]'),
+  document.querySelectorAll('[data-nav-link]'),
+  document.querySelector('[data-navbar]'),
+  document.querySelector('[data-overlay]'),
+];
+
+const toggleNav = function () {
+  navbar.classList.toggle('active'); // toggle active class on navbar
+  overlay.classList.toggle('active'); // toggle active class on overlay
+  document.body.classList.toggle('active'); // toggle active class on body
+};
+
+addEventOnElements(navTogglers, 'click', toggleNav); // add click event to navTogglers
+
+const closeNav = function () {
+  navbar.classList.remove('active'); // remove active class from navbar
+  overlay.classList.remove('active'); // remove active class from overlay
+  document.body.classList.remove('active'); // remove active class from body
+};
+
+addEventOnElements(navLinks, 'click', closeNav); // add click event to navLinks
+
+// Header
+const header = document.querySelector('[data-header]'); // get header element
+
+const activeElementOnScroll = function () {
+  if (window.scrollY > 50) {
+    header.classList.add('active'); // add active class to header
+  } else {
+    header.classList.remove('active'); // remove active class from header
+  }
+};
+
+window.addEventListener('scroll', activeElementOnScroll); // add scroll event to window
