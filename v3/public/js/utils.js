ab7cbfba8e50dea80ac7985c4fdf8179e3d1e600
AyomideKayode <ayomidekay7@gmail.com>
2025-02-25 13:40:52 +0100

    Add utility functions for event handling and preloading animation in utils.js.🚧

---
+++ v3/public/js/utils.js
@@ -0,0 +1,19 @@
+'use strict';
+// script for helper functions and utilities
+
+// add event listeners to multiple elements
+export const addEventOnElements = (elements, eventType, callback) => {
+  elements.forEach((element) => {
+    element.addEventListener(eventType, callback);
+  });
+};
+
+// Preloading Animation Effect
+export const initPreloader = () => {
+  const loadingElement = document.querySelector('[data-loading]');
+
+  window.addEventListener('load', function () {
+    loadingElement.classList.add('loaded'); // add loaded class to loading element
+    this.document.body.classList.remove('active'); // remove active class from body
+  });
+};
