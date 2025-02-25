ba6fc2ced9b0aff2ea09773a029076626b55e206
AyomideKayode <ayomidekay7@gmail.com>
2025-02-25 14:01:40 +0100

    Refactor script.js to use module imports for improved organization; remove deprecated functions and initialize utilities for preloading, navigation, and header scroll effects in index.html.🚧

--- v3/public/js/script.js
+++ v3/public/js/script.js
@@ -1,63 +1,16 @@
 'use strict'; // use strict mode meaning that the code should be executed in strict mode
 
-// add event listeners to multiple elements
-const addEventOnElements = function (elements, eventType, callback) {
-  for (let i = 0; i < elements.length; i++) {
-    // loop through all elements
-    elements[i].addEventListener(eventType, callback); // add event listener to each element
-  }
-};
+// import helper functions and utilities
+import { addEventOnElements } from "./utils.js";
+import { initPreloader } from "./utils.js";
+import { initNavbar } from "./navigation.js";
+import { initHeaderOnScroll } from "./navigation.js";
+import { initBackToTop } from "./navigation.js";
 
-/**
- * Preloading Animation Effect
- */
-const loadingElement = document.querySelector('[data-loading]'); // get loading element
-
-window.addEventListener('load', function () {
-  loadingElement.classList.add('loaded'); // add loaded class to loading element
-  this.document.body.classList.remove('active'); // remove active class from body
-});
-
-/**
- * Nav Toggle Functionality for Mobile
- */
-const [navTogglers, navLinks, navbar, overlay] = [
-  document.querySelectorAll('[data-nav-toggler]'),
-  document.querySelectorAll('[data-nav-link]'),
-  document.querySelector('[data-navbar]'),
-  document.querySelector('[data-overlay]'),
-];
-
-const toggleNav = function () {
-  navbar.classList.toggle('active'); // toggle active class on navbar
-  overlay.classList.toggle('active'); // toggle active class on overlay
-  document.body.classList.toggle('active'); // toggle active class on body
-};
-
-addEventOnElements(navTogglers, 'click', toggleNav); // add click event to navTogglers
-
-const closeNav = function () {
-  navbar.classList.remove('active'); // remove active class from navbar
-  overlay.classList.remove('active'); // remove active class from overlay
-  document.body.classList.remove('active'); // remove active class from body
-};
-
-addEventOnElements(navLinks, 'click', closeNav); // add click event to navLinks
-
-/**
- * Header Active Class on Scroll
- */
-const header = document.querySelector('[data-header]'); // get header element
-
-const activeElementOnScroll = function () {
-  if (window.scrollY > 50) {
-    header.classList.add('active'); // add active class to header
-  } else {
-    header.classList.remove('active'); // remove active class from header
-  }
-};
-
-window.addEventListener('scroll', activeElementOnScroll); // add scroll event to window
+initPreloader(); // call initPreloader function
+initNavbar(); // call initNavbar function
+initHeaderOnScroll(); // call initHeaderOnScroll function
+initBackToTop(); // call initBackToTop function
 
 /**
  * Text Animation Effect for HERO Section
@@ -137,27 +90,6 @@ const setLetterEffect = function () {
 window.addEventListener('load', setLetterEffect);
 
 /**
- * Back to Top Button
- */
-const backToTopBtn = document.querySelector('[data-back-top-btn]'); // get back to top button
-
-window.addEventListener('scroll', function () {
-  const bodyHeight = document.body.scrollHeight; // get body height
-  const windowHeight = window.innerHeight; // get window height
-  const scrollEndPos = bodyHeight - windowHeight; // get scroll end position
-  const totalScrollPercent = (window.scrollY / scrollEndPos) * 100; // get total scroll percentage
-
-  backToTopBtn.textContent = `${totalScrollPercent.toFixed(0)}%`; // set total scroll percentage on back to top button
-
-  // show back to top button when total scroll percentage is greater than 5
-  if (totalScrollPercent > 5) {
-    backToTopBtn.classList.add('show'); // add show class to back to top button
-  } else {
-    backToTopBtn.classList.remove('show'); // remove show class from back to top button
-  }
-});
-
-/**
  * Scroll Reveal Animation
  */
 const revealElements = document.querySelectorAll('[data-reveal]'); // get all reveal elements
@@ -205,6 +137,17 @@ document.addEventListener('DOMContentLoaded', function () {
       './images/category-2.jpg',
       './images/category-3.jpg',
       // more images...
+    ],
+    'The Art of Style': [
+      './images/potraits/potrait_1.png',
+      './images/potraits/potrait_5.png',
+      './images/potraits/potrait_6.png',
+      // more images...
+    ],
+    'Happily Ever After in Frames': [
+      './images/potraits/wedding_1.jpg',
+      './images/potraits/wedding_2.jpg',
+      './images/potraits/wedding_3.jpg',
     ],
   };
 
