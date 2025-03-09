1854356658e045fce2c05515a8dc4b1d8398de5c
AyomideKayode <ayomidekay7@gmail.com>
2025-03-09 02:57:59 +0100

    Add new Grublane and 12 Baskets images, and update event photography collection.🚧

--- v3/public/js/populateGallery.js
+++ v3/public/js/populateGallery.js
@@ -20,17 +20,53 @@ document.addEventListener('DOMContentLoaded', function () {
       './images/wedding-photography/wedding_3.jpg',
     ],
     'event-photography': [
-      './images/event-photography/12baskets_5.jpg',
-      './images/event-photography/12baskets_6.jpg',
-      './images/event-photography/12baskets_7.jpg',
+      './images/event-photography/event_1.jpg',
+      './images/event-photography/event_2.jpg',
+      './images/event-photography/event_3.jpg',
       './images/event-photography/12baskets_8.jpg',
       './images/event-photography/12baskets_9.jpg',
       './images/event-photography/12baskets_10.jpg',
+      './images/event-photography/event_4.jpg',
+      './images/event-photography/event_5.jpg',
+      './images/event-photography/event_6.jpg',
+      './images/event-photography/12baskets_5.jpg',
+      './images/event-photography/12baskets_6.jpg',
+      './images/event-photography/12baskets_7.jpg',
+      './images/event-photography/event_10.jpg',
+      './images/event-photography/event_15.jpg',
+      './images/event-photography/event_12.jpg',
+      './images/event-photography/event_7.jpg',
+      './images/event-photography/event_8.jpg',
+      './images/event-photography/event_9.jpg',
+      './images/event-photography/event_13.jpg',
+      './images/event-photography/event_11.jpg',
+      './images/event-photography/event_17.jpg',
+      './images/event-photography/event_14.jpg',
+      './images/event-photography/event_19.jpg',
+      './images/event-photography/event_16.jpg',
+      './images/event-photography/event_18.jpg',
+      './images/event-photography/event_22.jpg',
+      './images/event-photography/event_20.jpg',
+      './images/event-photography/event_21.jpg',
+      './images/event-photography/event_23.jpg',
+      './images/event-photography/event_24.jpg',
+      './images/event-photography/event_25.jpg',
+      './images/event-photography/event_26.jpg',
+      './images/event-photography/event_27.jpg',
     ],
     'product-marketing': [
-      './images/product/product_1.jpg',
-      './images/product/product_2.jpg',
-      './images/product/product_3.jpg',
+      './images/product/product_marketing_1.jpg',
+      './images/product/product_marketing_2.jpg',
+      './images/product/product_marketing_3.jpg',
+      './images/product/product_marketing_4.jpg',
+      './images/product/product_marketing_5.jpg',
+      './images/product/product_marketing_6.jpg',
+      './images/product/product_marketing_7.jpg',
+      './images/product/product_marketing_8.jpg',
+      './images/product/product_marketing_9.jpg',
+      './images/product/product_marketing_10.jpg',
+      './images/product/product_marketing_11.jpg',
+      './images/product/product_marketing_12.jpg',
     ],
     videography: [
       './images/video/video_1.jpg',
@@ -51,25 +87,4 @@ document.addEventListener('DOMContentLoaded', function () {
   } else {
     console.error('Category not found');
   }
-
-  // const categoryPath = `/images/${category}`;
-  // fetch(categoryPath)
-  //   .then((response) => {
-  //     if (!response.ok) {
-  //       throw new Error('Category not found');
-  //     }
-  //     return response.json();
-  //   })
-  //   .then((images) => {
-  //     images.forEach((src) => {
-  //       const img = document.createElement('img');
-  //       img.src = `${categoryPath}/${src}`;
-  //       img.alt = category;
-  //       img.loading = 'lazy';
-  //       gallery.appendChild(img);
-  //     });
-  //   })
-  //   .catch((error) => {
-  //     console.error('Error fetching images:', error);
-  //   });
 });
